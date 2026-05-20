25430e59b7a964daa9b5a130e43bf71814d1fb6c
Adam Manning <manning.adam@icloud.com>
2026-05-20 12:56:54 -0700

    Fix config and references

--- src/main.jsx
+++ src/main.jsx
@@ -20,7 +20,7 @@ import './styles/mobile-menu.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <App />
     </BrowserRouter>
   </React.StrictMode>,
--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/kato8-staging/',
   plugins: [react()],
   build: {
     outDir: 'docs',
